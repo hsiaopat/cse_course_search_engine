494653b8637ac7cf12e66ce412979c7f8c5b3a98
hsiaopat <94072513+hsiaopat@users.noreply.github.com>
2022-04-14 00:28:31 -0400

    Test to access database
    I may get a privacy warning but you need to be on a certain ip address anyway to access it.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pymongo
 import pprint
 
 def retrieve_db():
-	#hi
+	client = pymongo.MongoClient("mongodb://hsiaopat:Aerodynamicfeathers7@hsiaoer-of-pattys-shard-00-00.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-01.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-02.qdmrj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pyw8am-shard-0&authSource=admin&retryWrites=true&w=majority")
 	cse_db = client["cse_courses"]
 	cse_coll = cse_db["cse_collections"]
 	return cse_coll
