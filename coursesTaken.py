d0cc20da0843387cdd05f67561899add97a05fbd
hsiaopat <94072513+hsiaopat@users.noreply.github.com>
2022-04-20 17:23:54 -0400

    update to pseudocode

--- coursesTaken.py
+++ coursesTaken.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 #search
+
+def initialization():
+	#make two hashmaps of classes whether it is required or not
+	#all with taken value as 0
 def reqCourseTakenSearch(cse_coll,classTaken):#cse_coll is collection, classTaken is input
 	#classTaken is input 
 	classTakenList = []
 	#loop thru collection
 	#if classTaken in dictionary[i]
 		#append dictionary.key() to classTakenList
+		#in hashmap for required course, set taken value to 1
 	return classTakenList
 
 def electiveTakenSearch(cse_coll,classTaken):
@@ -14,6 +19,7 @@ def electiveTakenSearch(cse_coll,classTaken):
 	#loop thru collection
 	#if classTaken in dict
 		#append dictionary.key() to electiveTakenList
+		#in hashmap for electives, set taken value to 0
 	return electiveTakenList
 
 
