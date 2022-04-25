a0a8abd332fac68772b61beb402d03b5630313a2
Andrew Hennessee <ahennes3@student10.cse.nd.edu>
2022-04-24 20:25:45 -0400

    commented

--- electivesForYou.py
+++ electivesForYou.py
@@ -3,64 +3,83 @@
 import pymongo
 import pprint
 
-def retrieve_db():
+# fetch database
+def retrieveDB():
+	# make request
 	client = pymongo.MongoClient("mongodb://hsiaopat:Aerodynamicfeathers7@hsiaoer-of-pattys-shard-00-00.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-01.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-02.qdmrj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pyw8am-shard-0&authSource=admin&retryWrites=true&w=majority")
-	cse_db = client["cse_courses"]
-	cse_coll = cse_db["cse_collections"]
-	return cse_coll
+	# access database
+	cseDB = client["cse_courses"]
+	# access collection
+	cseColl = cseDB["cse_collections"]
 
-def initTaken(cse_coll): #create dictionary for required courses
-	#go thru each hashmap in collection
+	return cseColl
+
+# initialize dictionary to track taken courses
+def initTaken(cseColl):
 	takenDict = {}
-	for doc in cse_coll.find():
+	# iterate through database documents and update dictionary
+	for doc in cseColl.find():
 		number = doc["number"]
 		takenDict.update({number:0})
+
 	return takenDict
 
-def courseTakenSearch(cse_coll, course, takenDict):#cse_coll is collection, classTaken is input String
-	#loop thru collection
-	for doc in cse_coll.find():
-		#if classtaken is in dictionary, append the title to classtakenList, set taken value to 1 and then return classTakenList
+# update taken courses dictionary based on user input
+def courseTakenSearch(cseColl, course, takenDict):
+	# iterate through database
+	for doc in cseColl.find():
+		# search for user's course in course number/title and update if found
 		if course.lower() in doc["title"].lower() and doc["number"] in takenDict:
 			takenDict[doc["number"]] = 1
 		elif course in doc["number"] and doc["number"] in takenDict:
 			takenDict[doc["number"]] = 1
+
 	return takenDict
 
-def search_keyword(keyword, cse_coll, found_keyword):
-
-	for doc in cse_coll.find():
-		if doc in found_keyword:
+# search for keywork in course entries
+def searchKeyword(keyword, cseColl, foundKeyword):
+	# iterate through database
+	for doc in cseColl.find():
+		# check if course is already in list
+		if doc in foundKeyword:
 			pass
+		# append if keyword is found in description/title/number
 		elif keyword in doc["description"].lower() and doc["type"] == "Elective":
-			found_keyword.append(doc)
+			foundKeyword.append(doc)
 		elif keyword in doc["title"].lower() and doc["type"] == "Elective":
-			found_keyword.append(doc)
+			foundKeyword.append(doc)
 		elif keyword in doc["number"] and doc["type"] == "Elective":
-			found_keyword.append(doc)
+			foundKeyword.append(doc)
 
-	return found_keyword
+	return foundKeyword
 
-def checkPrereq(found_keyword, takenDict):
-
+# cross check user's taken courses with prereqs of electives
+def checkPrereq(foundKeyword, takenDict):
 	availableElectives = []
-
-	for course in found_keyword:
+	# iterate through list of electives with keyword(s)
+	for course in foundKeyword:
+		# check for prereqs attribute
 		try:
 			available = False
+			# iterate through prereqs
 			for prereq in course["prerequisites"]:
-				if not isinstance(prereq, str):
+				# check element class
+				# if the element is a single string, it is a required prereq
+				# (i.e., the case of XX and YY non-optionally)
+				if isinstance(prereq, str):
+					if takenDict[prereq] == 1:
+						available = True
+				# if the element is not a single string, it must be a list
+				# of strings (i.e., the case of XX or YY optionally)
+				else:
 					for orPrereq in prereq:
 						if takenDict[orPrereq] == 1:
 							available = True
 							break
-				else:
-					if takenDict[prereq] == 1:
-						available = True
-
+			# append elective if user meets prereqs
 			if available:
 				availableElectives.append(course)
-
+		# if no prereq attribute, append elective
 		except KeyError:
 			availableElectives.append(course)
 
@@ -68,33 +87,38 @@ def checkPrereq(found_keyword, takenDict):
 
 
 def main():
-	cse_coll = retrieve_db()
+	# retrieve database of course entries
+	cseColl = retrieveDB()
 
-	takenDict = initTaken(cse_coll)
-	takenCourses = "20289, 20221, Data Science, CSE Service Projects"
+	# create dictionary for taken courses
+	takenDict = initTaken(cseColl)
+	takenCourses = "20289, 20221, Data Science, CSE Service Projects" # test string
 	takenCourses = takenCourses.split(",")
 	for course in takenCourses:
 		course = course.strip()
-		takenDict = courseTakenSearch(cse_coll, course, takenDict)
+		takenDict = courseTakenSearch(cseColl, course, takenDict)
 
-	pprint.pprint(takenDict)
-	print(len(takenDict))
+	# REMOVE
+	#pprint.pprint(takenDict)
+	#print(len(takenDict))
 
-	keywords = "machine learning, 30125, database"
+	# search for keywords
+	keywords = "machine learning, 30125, database" # test string
 	keywords = keywords.split(",")
-	found_keyword = []
+	foundKeyword = []
 	for key in keywords:
 		key = key.strip()
-		found_keyword = search_keyword(key, cse_coll, found_keyword)
+		foundKeyword = searchKeyword(key, cseColl, foundKeyword)
 
-	for course in found_keyword:
-		print(f'{course["title"]}')
+	# REMOVE
+	#for course in foundKeyword:
+	#	print(f'{course["title"]}')
 
-	print('')
-	availableElectives = checkPrereq(found_keyword, takenDict)
+	#print('')
+	# check prereqs of found electives
+	availableElectives = checkPrereq(foundKeyword, takenDict)
 	for course in availableElectives:
 		print(f'{course["title"]}')
-
 
 
 if __name__ == '__main__':
