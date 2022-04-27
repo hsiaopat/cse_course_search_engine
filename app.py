d0eec2950244c9b4f1dfb2a584b0a4b6f1f03a8c
Andrew Hennessee <ahennes3@student05.cse.nd.edu>
2022-04-27 00:56:30 -0400

    works

--- app.py
+++ app.py
@@ -1,92 +1,46 @@
 import requests
 import os
 import sys
-from flask import Flask, render_template, request, jsonify
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request
+import pymongo
+import pprint
 
 app = Flask(__name__)
 
 # retrieve info from database
-#def retrieve_db():
-#	# make request
-#	client = PyMongo(app,"mongodb://hsiaopat:Aerodynamicfeathers7@hsiaoer-of-pattys-shard-00-00.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-01.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-02.qdmrj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pyw8am-shard-0&authSource=admin&retryWrites=true&w=majority")
-#	client_db = client.db
-#	db = client_db["cse_courses"]
-#	# access database
-#	#db = client["cse_courses"]
-#	# access collection
-#	cse_coll = db
-#
-#	return cse_coll
-#	#return jsonify([todo for todo in todos])
-
-
-
-@app.route("/",methods=["POST","GET"])
-# retrieve info from database
-def retrieve_db():
+def retrieveDB():
 	# make request
-	client = PyMongo(app,uri="mongodb://hsiaopat:Aerodynamicfeathers7@hsiaoer-of-pattys-shard-00-00.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-01.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-02.qdmrj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pyw8am-shard-0&authSource=admin&retryWrites=true&w=majority")
-	db = client.db
+	client = pymongo.MongoClient("mongodb://hsiaopat:Aerodynamicfeathers7@hsiaoer-of-pattys-shard-00-00.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-01.qdmrj.mongodb.net:27017,hsiaoer-of-pattys-shard-00-02.qdmrj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pyw8am-shard-0&authSource=admin&retryWrites=true&w=majority")
 	# access database
-	cse_db = db["cse_courses"]
+	cseDB = client["cse_courses"]
 	# access collection
-	cse_coll = cse_db["cse_collections"]
-	cseDocs = cse_coll.find()
+	cseColl = cseDB["cse_collections"]
 
-#	#return cse_coll
-#	todos = db.todos.find()
-#	print(todos)
-	return cseDocs #jsonify([cseDoc for cseDoc in cseDocs])
+	return cseColl
 
-#@app.route("/",methods=["POST","GET"])
-def index():
+# create dictionary for required courses
+def initRequired(cse_coll):
+	requiredDict = {}
+	# iterate through each hashmap in collection
+	for doc in cse_coll.find():
+		# append all titles with type required with a taken value of 0
+		if doc["type"] == "Required":
+			name = doc["title"]
+			requiredDict.update({name:0})
 
-	cseColl = retrieve_db()
-	print(cseColl)
+	return requiredDict
 
-	takenDict = initTaken(cseColl)
-	print(takenDict)
-	requiredTaken = initRequired(cseColl)
-	electiveTaken = initElective(cseColl)
-	requiredTaken = ["Fundamentals of Computing", "Discrete Math"]
-	requiredRemaining = ["Discrete Math", "Logic Design"]
-	electives = []
+# search through database for required courses listed in user input, update dictionary
+def reqCourseTakenSearch(cse_coll, classTaken, requiredDict):
+	# iterate through collection
+	for doc in cse_coll.find():
+		# if classtaken is in dictionary, append the title to classtakenList, set taken value to 1 and then return classTakenList
+		if classTaken.lower() in doc["title"].lower() and doc["title"] in requiredDict:
+			requiredDict[doc["title"]] = 1
+		elif classTaken in doc["number"] and doc["title"] in requiredDict:
+			requiredDict[doc["title"]] = 1
 
-	if request.method == "GET": #GET request is sent when html wants some information from python backend
-		print(cseColl)
-		return render_template("index.html", taken=requiredTaken, remaining=requiredRemaining, electivesAvailable=electives)
-
-	if request.method == "POST": #POST request is when the webpage is sending data to python backend
-
-		courseInput = request.form.get("courseInput")
-		keywordInput = request.form.get("keywordInput")
-		requiredTaken = reqCourseTakenSearch(cseColl, courseInput, requiredTaken)
-		electives = findElectives(courseInput, keywordInput, cseColl, takenDict)
-
-	return render_template("index.html", taken=requiredTaken, remaining=requiredRemaining, electivesAvailable=electives)
-
-
-def findElectives(courseInput, keywordInput, cseColl, takenDict):
-
-	courseInput = courseInput.split()
-	for course in courseInput:
-		course = course.strip()
-		takenDict = courseTakenSearch(cseColl, course, takenDict)
-
-	keywordInput = keywordInput.split(",")
-	foundKeyword = []
-	for key in keywordInput:
-		key = key.strip()
-		foundKeyword = searchKeyword(key, cseColl, foundKeyword)
-
-	availableElectives = checkPrereq(foundKeyword, takenDict)
-	electives = []
-	for course in availableElectives:
-		electives.append(course["title"])
-
-	return electives
-
+	return requiredDict
 
 
 # initialize dictionary to track taken courses
@@ -152,69 +106,76 @@ def checkPrereq(foundKeyword, takenDict):
 							available = True
 							break
 			# append elective if user meets prereqs
-			if available:
+			if available and takenDict[course["number"]] == 0:
 				availableElectives.append(course)
 		# if no prereq attribute, append elective
 		except KeyError:
-			availableElectives.append(course)
+			if takenDict[course["number"]] == 0:
+				availableElectives.append(course)
 
 	return availableElectives
 
-# create dictionary for required courses
-def initRequired(cseColl):
-	requiredDict = {}
-	# iterate through each hashmap in collection
-	for doc in cseColl.find():
-		# append all titles with type required with a taken value of 0
-		if doc["type"] == "Required":
-			name = doc["title"]
-			requiredDict.update({name:0})
+def findRequired(courseInput, cseColl, requiredDict):
 
-	return requiredDict
+	for course in courseInput:
+		course = course.strip()
+		if not course == "":
+			requiredDict = reqCourseTakenSearch(cseColl, course, requiredDict)
 
-# create dictionary for elective courses
-def initElective(cseColl):
-	electiveDict = {}
-	# iterate through each hashmap in collection
-	for doc in cseColl.find():
-		# append all titles with type electives with a taken value of 0
-		if doc["type"] == "Elective":
-			name = doc["title"]
-			electiveDict.update({name:0})
+	requiredTaken = []
+	requiredRemaining = []
+	for course, taken in requiredDict.items():
+		if taken:
+			requiredTaken.append(course)
+		else:
+			requiredRemaining.append(course)
 
-	return electiveDict
+	return requiredTaken, requiredRemaining
 
-# search through database for required courses listed in user input, update dictionary
-def reqCourseTakenSearch(cseColl, classTaken, requiredDict):
-	count = 0
-	# iterate through collection
-	for doc in cseColl.find():
-		# if classtaken is in dictionary, append the title to classtakenList, set taken value to 1 and then return classTakenList
-		if classTaken.lower() in doc["title"].lower() and doc["title"] in requiredDict:
-			requiredDict[classTaken] = 1
-		elif classTaken in doc["number"] and doc["title"] in requiredDict:
-			requiredDict[doc["title"]] = 1
-	'''
-	for key,value in requiredDict.items():
-		if(value == 1):
-			reqTakenList.append(key)
-	'''
+def findElectives(courseInput, keywordInput, cseColl, takenDict):
 
-	return requiredDict
+	for course in courseInput:
+		course = course.strip()
+		takenDict = courseTakenSearch(cseColl, course, takenDict)
 
-# search through database for elective courses listed in user input, update dictionary
-def electiveTakenSearch(cseColl, classTaken, electiveDict):
-	# loop thru collection
-	for doc in cseColl.find():
-		# if classTaken in dict, append dictionary.key() to electiveTakenList and in hashmap for electives, set taken value to 0
-		if classTaken.lower() in doc["title"].lower() and doc["title"] in electiveDict:
-			electiveDict[classTaken] = 1
-		elif classTaken in doc["number"] and doc["title"] in electiveDict:
-			electiveDict[doc["title"]] = 1
-	'''
-	for key,value in electiveDict.items():
-		if(value == 1):
-			electiveTakenList.append(key)
-	'''
+	foundKeyword = []
+	for key in keywordInput:
+		key = key.strip()
+		foundKeyword = searchKeyword(key, cseColl, foundKeyword)
 
-	return electiveDict
+	availableElectives = checkPrereq(foundKeyword, takenDict)
+
+	electives = []
+	for course in availableElectives:
+		electives.append(course["title"])
+
+	return electives
+
+@app.route("/",methods=["POST","GET"])
+def index():
+
+	cseColl = retrieveDB()
+	takenDict = initTaken(cseColl)
+	requiredDict = initRequired(cseColl)
+
+	requiredTaken = []
+	requiredRemaining = []
+	electives = []
+
+	if request.method == "GET": #GET request is sent when html wants some information from python backend
+
+		return render_template("index.html", taken=requiredTaken, remaining=requiredRemaining, electivesAvailable=electives)
+
+	if request.method == "POST": #POST request is when the webpage is sending data to python backend
+
+		courseInput = request.form.get("courseInput")
+		courseInput = courseInput.split(",")
+
+		keywordInput = request.form.get("keywordInput")
+		keywordInput = keywordInput.split(",")
+
+		requiredTaken, requiredRemaining = findRequired(courseInput, cseColl, requiredDict)
+		electives = findElectives(courseInput, keywordInput, cseColl, takenDict)
+
+
+	return render_template("index.html", taken=requiredTaken, remaining=requiredRemaining, electivesAvailable=electives)
